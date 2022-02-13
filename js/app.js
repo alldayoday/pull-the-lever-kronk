167ece827ec85985a8d7c08e24c92809157e1c15
Erin ODea <odea.erin@gmail.com>
2022-02-13 09:07:06 -0600

    readme

--- js/app.js
+++ js/app.js
@@ -1,39 +1,38 @@
-
+/**---CACHED ELEMENT REF---**/
 const pull = document.querySelector('#lever')
 const num1 = document.querySelector('#num1')
 const num2 = document.querySelector('#num2')
-const num3 = document.querySelector('#num3'
-)
+const num3 = document.querySelector('#num3')
 const h1 = document.querySelector('#title')
 const credit = document.querySelector('#credit')
 const lightDarkBtn = document.querySelector("#light-dark-button")
 const body = document.querySelector("body")
+
+/**---AUDIO FILES---**/
 const allComingTogether = new Audio('../assets/all-coming-together.mp3')
 const wrongLever = new Audio('../assets/wrong-lever.mp3')
 const whyThatLever = new Audio('../assets/why-do-we.mp3')
 
-// let prettyNum
+/**---VARIABLES---**/
 let creditTotal = 50
+
+/**---EVENT LISTENERS---**/
 pull.addEventListener('click', checkCredit)
 lightDarkBtn.addEventListener("click", toggleLightDark)
 
-function toggleLightDark() {
-  body.className = body.className === "dark" ? "" : "dark"
-  h1.className = h1.className === "dark" ? "" : "dark"
-  if (body.className === "dark") {
-    lightDarkBtn.textContent = "KRONK"
-  } else {
-    lightDarkBtn.textContent = "BEES?"
-  }
+
+init()
+function init() {
+  disp1 = Math.floor(Math.random() * 10 + 1)
+  disp2 = Math.floor(Math.random() * 10 + 1)
+  disp3 = Math.floor(Math.random() * 10 + 1)
+  num1.innerHTML = `<img src="../assets/${disp1}.png" />`
+  num2.innerHTML = `<img src="../assets/${disp2}.png" />`
+  num3.innerHTML = `<img src="../assets/${disp3}.png" />`
 }
 
-// init()
-// function init (){
-//   startSpin()
-// }
 
-
-function checkCredit(){
+function checkCredit() {
   if (creditTotal >= 5) {
     creditTotal = creditTotal -= 5
     credit.textContent = `${creditTotal} Credits`
@@ -44,20 +43,21 @@ function checkCredit(){
 }
 
 
-function renderNum (){
-  secretNum1 = Math.floor(Math.random() * 3 +1)
+function renderNum() {
+  secretNum1 = Math.floor(Math.random() * 2 + 1)
   num1.setAttribute("class", "roll-in-top")
   num1.innerHTML = `<img src="../assets/${secretNum1}.png" />`
-  secretNum2 = Math.floor(Math.random() * 3 +1)
+  secretNum2 = Math.floor(Math.random() * 2 + 1)
   num2.setAttribute("class", "roll-in-top2")
   num2.innerHTML = `<img src="../assets/${secretNum2}.png" />`
-  secretNum3 = Math.floor(Math.random() * 3 +1)
+  secretNum3 = Math.floor(Math.random() * 2 + 1)
   num3.setAttribute("class", "roll-in-top3")
   num3.innerHTML = `<img src="../assets/${secretNum3}.png" />`
   checkWin()
 }
 
-function checkWin(){
+
+function checkWin() {
   if (secretNum1 === secretNum2 && secretNum1 === secretNum3) {
     h1.innerText = `Winner!`
     confetti.start(3000)
@@ -67,7 +67,7 @@ function checkWin(){
     allComingTogether.play()
   } else if (creditTotal > 999) {
     h1.innerText = `You should honestly just cash out now`
-  } else if (creditTotal > 0){
+  } else if (creditTotal > 0) {
     h1.innerText = `WRONG LEVEEEEER!!`
     wrongLever.volume = .15
     wrongLever.play()
@@ -75,80 +75,24 @@ function checkWin(){
     h1.innerText = `Why do we even have that lever?`
     whyThatLever.volume = .15
     whyThatLever.play()
-  } 
-  setTimeout(stopFlip, 1100)
+  }
+  setTimeout(stopSpin, 1100)
 }
-// foxSays.volume = .10
-//   foxSays.play()
 
-function stopFlip(){
+
+function stopSpin() {
   num1.setAttribute("class", "no-spin")
   num2.setAttribute("class", "no-spin")
   num3.setAttribute("class", "no-spin")
 }
 
-// let timeLeft = 5
 
-// function startSpin(){
-//   spin = setInterval(roll, 200)
-// }
-// function seconds(){
-
-// }
-
-init()
-function init() {
-  disp1 = Math.floor(Math.random() * 10 +1)
-  disp2 = Math.floor(Math.random() * 10 +1)
-  disp3 = Math.floor(Math.random() * 10 +1)
-  num1.innerHTML = `<img src="../assets/${disp1}.png" />`
-  num2.innerHTML = `<img src="../assets/${disp2}.png" />`
-  num3.innerHTML = `<img src="../assets/${disp3}.png" />`
+function toggleLightDark() {
+  body.className = body.className === "dark" ? "" : "dark"
+  h1.className = h1.className === "dark" ? "" : "dark"
+  if (body.className === "dark") {
+    lightDarkBtn.textContent = "KRONK"
+  } else {
+    lightDarkBtn.textContent = "BEES?"
+  }
 }
-
-
-
-
-// function slotStyle(secretNum){
-//   if (secretNum === 1) prettyNum = " 🍒 "
-//   if (secretNum === 2) prettyNum = " 🍄 "
-//   if (secretNum === 3) prettyNum = " 🍀 "
-//   if (secretNum === 4) prettyNum = " 🦄 "
-//   if (secretNum === 5) prettyNum = " 🌈 "
-//   if (secretNum === 6) prettyNum = " ♥️ "
-//   if (secretNum === 7) prettyNum = " 🍩 "
-//   if (secretNum === 8) prettyNum = " 🌮 "
-//   if (secretNum === 9) prettyNum = " 🧁️ "
-//   if (secretNum === 10) prettyNum = " 🍌 "
-// }
-
-
-//pull the lever, kronk !
-//an emperors new groove themed slot machine? maybe!
-
-//each time a user clicks to pull the lever, call credit function to check if credit total is above 5 credits, if not, set h1 to say user is broke and game end✅
-
-//if credit total is above 5 credits, subtract 5 credits from credit total, update credit box, call generate function✅
-
-//for generate function, generate 3 random numbers (lets shoot for 1-10)✅
-
-//for window, tie each of the random numbers generated to an image (maybe emoji) and place one in each box then call win function✅
-
-//for win function if num1 is the same as num2 and also the same as num3, win condition is met and credits are added to total.✅
-
-//add a favicon ✅
-
-//add animation to make it look like images/emojis are "rolling" within their boxes, if not possible try some sort of slow fade in with a timeout on win function
-
-//add audio "cha-ching" when credit function runs and audio "crowd cheering" plus animation "confetti" on win
-
-//if possible find an animation to make it look like the lever arm moves on button click
-
-//stretch goal: if credits hit 0, instead of saying user is broke, remove hidden attribute from a form and list itemizing things the user can type in to sell for more credits. If they type in one of the options, add credits to credit total, hide form and list, and allow to keep playing. If they type in something not on the list h1 should say sorry, we dont accept those here but you can try the pawn shop down the road. This could also be done with multiple hidden buttons to avoid user typing incorrectly.
-
-//how to add audio
-//const foxSays = new Audio('../audio/fox.mp3')
-// foxImg.addEventListener("click", (evt) => {
-//   foxSays.volume = .10
-//   foxSays.play()
-// })
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Pull The Lever, Kronk!
+
+### This project is an *Emperor's New Groove* themed slot machine game. Inspired by the Disney Classic, this game allows the player to imagine they are in Kronk's shoes, attempting to pull the lever and hope that it all comes together. Simply click to pull the lever, and wait to see your result. I was inspired to create this game because I love *Emperor's New Groove* and any time I think of a lever, my mind goes straight to Kronk.
+
+## Game Deployment
+* https://pull-the-lever-kronk.surge.sh/
+* Please note that the title will say that you have pulled the wrong lever. This is not an indication that you have selected the wrong button to click, it is simply the correct reference to the film the game is based on. You should be clicking the button that says "Pull Lever".
+
+## Technologies Used:
+Project created with
+*HTML
+*CSS
+*JavaScript
+
+## Future Enchancements 
+*Add a pawn shop option where players can select an item in their posession to sell directly to the casino in order to keep playing.
+
+## Game Images
+![SS1KRONK](../pull-the-lever-kronk/assets/SS1.png)
+![SS2DARK](../pull-the-lever-kronk/assets/SS2.png)
